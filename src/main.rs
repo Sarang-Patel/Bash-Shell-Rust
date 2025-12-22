92ecd1afc10a391534b57d76a2c3cfe57c59f3e8
Sarang Ajay Patel <sarangp203@gmail.com>
2025-12-22 17:16:18 -0500

    getting inputs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    let mut cmd  = String::new();
+
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    io::stdin().read_line(&mut cmd).expect("failed to read line");
+
+    let cmd = cmd.trim();
+
+    println!("{cmd}: command not found");
 }
