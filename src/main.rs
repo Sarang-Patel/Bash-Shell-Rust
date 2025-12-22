b7b8327eed8fb7c83dc46dad32f9b25b72de0899
Sarang Ajay Patel <sarangp203@gmail.com>
2025-12-22 17:34:48 -0500

    repl

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn read_input(prompt: &str) -> String {
+    let mut input  = String::new();
+    let mut stdout = io::stdout();
+
+    print!("{prompt} ");
+    stdout.flush().unwrap();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    input.trim().to_string()
+
+}
+
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    let mut cmd  = String::new();
+    
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        let cmd = read_input("$");
+        println!("{cmd}: command not found");
+    }
 
-    io::stdin().read_line(&mut cmd).expect("failed to read line");
-
-    let cmd = cmd.trim();
-
-    println!("{cmd}: command not found");
 }
