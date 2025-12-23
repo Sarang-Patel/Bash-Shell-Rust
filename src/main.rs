fdcd5e10631ed318cb471a4b2b15ec55209c4697
Sarang Ajay Patel <sarangp203@gmail.com>
2025-12-22 23:57:59 -0500

    running external programs in path

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 use std::{env};
 use std::path::Path;
 use is_executable::IsExecutable;
+use std::process::Command;
 
 
 fn read_input(prompt: &str) -> String {
@@ -21,36 +22,65 @@ fn read_input(prompt: &str) -> String {
 
 fn main() {
         
-    let path_var = env::var("PATH").expect("PATH not set");
     let separator = if cfg!(windows) { ";" } else { ":" };
-
+    let builtin: HashSet<String> = ["exit", "echo", "type"].iter().map(|s| s.to_string()).collect();
+    
     loop {
+        let path_var = env::var("PATH").unwrap_or_default();
         let input = read_input("$");
 
-        let builtin: HashSet<_> = ["exit", "echo", "type"].iter().cloned().collect();
-        let mut parts = input.splitn(2, ' ');
-        let cmd = parts.next().unwrap_or("").trim();
-        let arg = parts.next().unwrap_or("").trim();
+        let parts: Vec<&str> = input.split_whitespace().collect();
 
+        let cmd: String = parts.get(0).unwrap_or(&"").to_string();
+        let args: Vec<String> = parts.iter().skip(1).map(|s| s.to_string()).collect();
 
-        match cmd {
-            "exit" => break,
-            "echo" => println!("{arg}"),
-            "type" => {
-                if builtin.contains(arg) {
-                    println!("{arg} is a shell builtin");
-                }else {
-                    if let Some(full_path) = path_var.split(separator).map(|dir| Path::new(dir).join(arg)).find(|p| p.exists() && p.is_executable()) {
-                        println!("{arg} is {}", full_path.display());
-                    }else{
-                        println!("{arg}: not found");
+        if builtin.contains(&cmd) {
+            match cmd.as_str() {
+                "exit" => break,
+                "echo" => println!("{}", args.join(" ")),
+                "type" => {
+                    if args.is_empty() {
+                        println!("type: missing operand");
+                        continue;
                     }
+
+                    for arg in &args {
+                    if builtin.contains(arg) {
+                        println!("{arg} is a shell builtin");
+                    }else {
+                        if let Some(full_path) = path_var.split(separator)
+                        .map(|dir| Path::new(dir).join(arg))
+                        .find(|p| p.exists() && p.is_executable()) {
+                            println!("{arg} is {}", full_path.display());
+                        }else{
+                            println!("{arg}: not found");
+                        }
+                    }}
                 }
+                _ => println!("{cmd}: command not found"),
             }
-            _ => println!("{cmd}: command not found"),
+        }else {
+            if let Some(_full_path) = path_var.split(separator).map(|dir| Path::new(dir).join(cmd.to_string()))
+            .find(|p| p.exists() && p.is_executable()) {
+                println!("{}", args.join(" "));
+                let output = Command::new(cmd).args(&args).output().expect("Failed to execute process");
+
+                let stdout = str::from_utf8(&output.stdout).unwrap();
+                let stderr = str::from_utf8(&output.stderr).unwrap();
+
+                if !stdout.is_empty() {
+                    print!("{}", stdout);
+                }
+
+                if !stderr.is_empty() {
+                    eprint!("{}", stderr);
+                }
+
+            }else{
+                println!("{cmd}: not found.");
+            }
         }
 
-        
     }
 
 }
