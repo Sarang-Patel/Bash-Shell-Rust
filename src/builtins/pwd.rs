0d9eda2223642be77f52c8c9399c2e20e19c71d3
Sarang <98686455+Sarang-Patel@users.noreply.github.com>
2025-12-31 00:13:49 -0500

    bug fix

--- src/builtins/pwd.rs
+++ src/builtins/pwd.rs
@@ -5,14 +5,18 @@ use std::io::Write;
 pub fn run(ctx: BuiltinContext) -> bool {
     match env::current_dir() {
         Ok(path) => {
-            ctx.builtin_out
-                .write_all(path.display().to_string().as_bytes())
-                .unwrap();
+            let _ = writeln!(
+                ctx.builtin_out,
+                "{}",
+                path.display()
+            );
         }
         Err(e) => {
-            ctx.builtin_err
-                .write_all(e.to_string().as_bytes())
-                .unwrap();
+            let _ = writeln!(
+                ctx.builtin_err,
+                "{}",
+                e
+            );
         }
     }
     true
